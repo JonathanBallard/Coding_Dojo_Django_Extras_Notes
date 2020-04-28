c4f36a2e42e740abc7a0474df5c1c1f43be47617
Jonathan Ballard <asgardschosen@gmail.com>
2020-04-27 17:36:39 -0700

    fixed forms.py

--- apps/note_app/forms.py
+++ apps/note_app/forms.py
@@ -3,8 +3,8 @@ from django import forms
 from .models import Note
 
 
-class NoteForm(forms.Form):
-    content = forms.CharField(max_length=1000)
+class NoteForm(forms.ModelForm):
+    # content = forms.CharField(max_length=1000)
     class Meta:
         model = Note
         fields = '__all__'
